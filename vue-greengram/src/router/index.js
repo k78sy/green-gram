8a13c0b1f48e9ea200457760d72fa85485cebae1
ksy <tndud4615@gmail.com>
2026-02-20 12:39:38 +0900

    first commit

---
+++ vue-greengram/src/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import SignUp from '@/views/SignUp.vue'
+import SignIn from '@/views/SignIn.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/sign-up',
+      component: SignUp,
+    },
+    {
+      path: '/sign-in',
+      component: SignIn,
+    },
+  ],
+})
+
+export default router
