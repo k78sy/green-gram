697331fb9131e56aa3d2b7c8f60f2e5cebbdca5c
ksy <tndud4615@gmail.com>
2026-02-23 11:18:17 +0900

    2026-02-23 profile 작업중

--- vue-greengram/src/router/index.js
+++ vue-greengram/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Feed from '../views/Feed.vue'
 import SignUp from '@/views/SignUp.vue'
 import SignIn from '@/views/SignIn.vue'
+import Profile from '@/views/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,6 +19,10 @@ const router = createRouter({
       path: '/sign-in',
       component: SignIn,
     },
+    {
+      path: '/profile/:userId',
+      component: Profile,
+    }
   ],
 })
 
