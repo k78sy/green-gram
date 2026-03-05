b7dbb929c87256d260820284a1ee2423296b17d6
ksy <tndud4615@gmail.com>
2026-03-05 11:19:23 +0900

    피드 코멘트 삭제 (피니아) 삭제

--- vue-greengram/src/stores/commentModal.js
+++ vue-greengram/src/stores/commentModal.js
@@ -97,13 +97,14 @@ export const useCommentModalStore = defineStore("commentModal", () => {
     if (!confirm("삭제하시겠습니까?")) {
       return;
     }
-    const params = { feed_comment_id: feedCommentId };
+    const params = { feed_comment_id: item.feedCommentId };
     const res = await deleteComment(params);
     if (res.status === 200) {
       const idx = state.commentList.indexOf(item);
-      state.commentList.splice(idx, 1);
+      state.commentList.splice(idx, 1); //배열에서 아이템 삭제하는 방법
+
       const feedStore = useFeedStore();
-      feedStore.commentCountDown(feedId);
+      feedStore.commentCountDown(item.feedId);
     }
   };
 
